c877103aad7e7289426b910bb6d0d51911f94895
Matthias <matthias.leys@belorta.be>
2024-12-16 15:59:43 +0100

    + dag 11 + deel 1 dag 12

---
+++ 11-Pluto.vb
@@ -0,0 +1,127 @@
+﻿Imports System.IO
+Imports System.Runtime.InteropServices
+Imports System.Threading
+Imports Microsoft.VisualBasic.FileIO
+
+
+Public Module Pluto
+
+    Dim MemoTable As New Dictionary(Of Long, (Long, Long))
+
+    Public Sub StenenTellen()
+        Dim fileName As String = "Pluto.txt"
+        Dim projectPath As String = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName
+        Dim pad As String = Path.Combine(projectPath, fileName)
+
+        Dim Stenen As New Dictionary(Of Long, Long)
+
+        Using parser = New TextFieldParser(pad)
+            parser.SetDelimiters(" ")
+
+            While Not parser.EndOfData
+                Dim fields As String() = parser.ReadFields()
+                For Each F In fields
+                    Stenen.Add(F, 1)
+                Next
+            End While
+
+            parser.Close()
+            parser.Dispose()
+        End Using
+
+
+        MemoTable.Add(0, (1, -1))
+
+        Console.WriteLine(BlinkSneller(Stenen, 75).ToString)
+
+
+    End Sub
+
+    Private Function BlinkSneller(Stenen As Dictionary(Of Long, Long), Blinks As Integer) As Long
+        Dim stopwatch As Stopwatch = Stopwatch.StartNew()
+
+        For i = 1 To Blinks
+            Dim NieuweStenen As New Dictionary(Of Long, Long)
+
+            For Each KVP In Stenen
+                Dim steen = KVP.Key
+                Dim value As (Long, Long)
+                If MemoTable.TryGetValue(steen, value) Then
+                    If value.Item2 <> -1 Then
+                        If Not NieuweStenen.TryAdd(value.Item2, KVP.Value) Then
+                            NieuweStenen(value.Item2) += KVP.Value
+                        End If
+                    End If
+                    If Not NieuweStenen.TryAdd(value.Item1, KVP.Value) Then
+                        NieuweStenen(value.Item1) += KVP.Value
+                    End If
+                Else
+                    Dim str = CStr(steen)
+                    Dim lengte = str.Length
+                    If lengte Mod 2 = 0 Then
+                        Dim midden = lengte / 2
+                        Dim deel1 As Long = str.Substring(0, midden)
+                        Dim deel2 As Long = str.Substring(midden, midden)
+                        MemoTable.Add(steen, (deel1, deel2))
+                        If Not NieuweStenen.TryAdd(deel1, KVP.Value) Then
+                            NieuweStenen(deel1) += KVP.Value
+                        End If
+                        If Not NieuweStenen.TryAdd(deel2, KVP.Value) Then
+                            NieuweStenen(deel2) += KVP.Value
+                        End If
+                    Else
+                        Dim nieuwnr = steen * 2024
+                        MemoTable.Add(steen, (nieuwnr, -1))
+                        If Not NieuweStenen.TryAdd(nieuwnr, KVP.Value) Then
+                            NieuweStenen(nieuwnr) += KVP.Value
+                        End If
+                    End If
+                End If
+            Next
+
+            Stenen = NieuweStenen
+        Next
+        Dim totaal As Long = 0
+
+        For Each N In Stenen.Values
+            totaal += N
+        Next
+        stopwatch.Stop()
+
+        Console.WriteLine("In " + stopwatch.ElapsedMilliseconds.ToString + "ms")
+        Return totaal
+    End Function
+
+
+    Private Function Blink(Stenen As List(Of String), Blinks As Integer) As Integer
+
+        For i = 1 To Blinks
+            Dim NieuweStenen As New List(Of String)
+
+            For Each S In Stenen
+                Dim nr As Long = Convert.ToInt64(S)
+                If nr = 0 Then
+                    NieuweStenen.Add("1")
+                ElseIf S.Length Mod 2 = 0 Then
+                    Dim midden As Integer = S.Length / 2
+                    NieuweStenen.Add(Convert.ToInt64(S.Substring(0, midden)))
+                    NieuweStenen.Add(Convert.ToInt64(S.Substring(midden, midden)))
+                Else
+                    NieuweStenen.Add(nr * 2024)
+                End If
+            Next
+
+            If i < 10 Then
+                For Each N In NieuweStenen
+                    Console.Write(N + " ")
+                Next
+
+            End If
+
+            Stenen = NieuweStenen
+            Console.WriteLine(i.ToString + " Blink(s) - " + Stenen.Count.ToString)
+        Next
+
+        Return Stenen.Count
+    End Function
+End Module
---
+++ 12-Garden.vb
@@ -0,0 +1,151 @@
+﻿Imports System.IO
+
+Module Garden
+    Private upperX As Integer
+    Private upperY As Integer
+    Private Map As New Dictionary(Of (x As Integer, y As Integer), Char)
+    Private areas As New Dictionary(Of Integer, Dictionary(Of (x As Integer, y As Integer), Integer))
+
+    Public Sub Perimeter()
+        Dim fileName As String = "Garden.txt"
+        Dim projectPath As String = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName
+        Dim pad As String = Path.Combine(projectPath, fileName)
+
+        Dim lijnen() As String = File.ReadAllLines(pad)
+
+        upperX = lijnen.Length() - 1
+        upperY = lijnen.First.Length() - 1
+
+        For x = 0 To upperX
+            Dim lijn = lijnen(x)
+            For y = 0 To upperY
+                Map.Add((x, y), lijn(y))
+            Next
+        Next
+
+        Dim area As Integer = 0
+        Dim totaal As Integer = 0
+
+        For Each N In Map
+            If N.Value = "/" Then Continue For
+            Dim subtotaal As Integer = 0
+            areas.Add(area, New Dictionary(Of (x As Integer, y As Integer), Integer))
+            Dim kanten = FindNeighboursAndHoeken(N.Key, area, -1)
+
+            For Each T In areas(area)
+                subtotaal += T.Value
+                Map(T.Key) = "/"
+            Next
+
+            Dim prijs As Integer = areas(area).Count * subtotaal
+            Dim prijsBulk As Integer = areas(area).Count * kanten
+
+            ' Console.WriteLine(N.Value + " - " + areas(area).Count.ToString + " * " + subtotaal.ToString + " = " + prijs.ToString)
+
+            Console.WriteLine(N.Value + " - " + areas(area).Count.ToString + " * " + kanten.ToString + " = " + prijsBulk.ToString)
+
+            totaal += prijs
+
+            area += 1
+        Next
+
+
+        Console.WriteLine(totaal)
+    End Sub
+
+    Private Function FindNeighboursAndHoeken(Node As (x As Integer, y As Integer), Area As Integer, richting As Short) As Integer
+        Dim aantalNeighbours As Integer = 0
+        Dim Teken As Char = Map(Node)
+        Dim hoeken As Integer = 0
+
+        If Node.x > 0 Then
+            If Map((Node.x - 1, Node.y)) = Teken Then
+                aantalNeighbours += 1
+                If areas(Area).TryAdd((Node.x - 1, Node.y), 0) Then
+                    hoeken += FindNeighboursAndHoeken((Node.x - 1, Node.y), Area, 0)
+                    If richting <> 0 Then
+                        hoeken += 1
+                    End If
+                End If
+            End If
+        End If
+        If Node.x < upperX Then
+            If Map((Node.x + 1, Node.y)) = Teken Then
+                aantalNeighbours += 1
+                If areas(Area).TryAdd((Node.x + 1, Node.y), 0) Then
+                    hoeken += FindNeighboursAndHoeken((Node.x + 1, Node.y), Area, 1)
+                    If richting <> 1 Then
+                        hoeken += 1
+                    End If
+                End If
+            End If
+        End If
+        If Node.y > 0 Then
+            If Map((Node.x, Node.y - 1)) = Teken Then
+                aantalNeighbours += 1
+                If areas(Area).TryAdd((Node.x, Node.y - 1), 0) Then
+                    hoeken += FindNeighboursAndHoeken((Node.x, Node.y - 1), Area, 2)
+                    If richting <> 2 Then
+                        hoeken += 1
+                    End If
+                End If
+            End If
+        End If
+        If Node.y < upperY Then
+            If Map((Node.x, Node.y + 1)) = Teken Then
+                aantalNeighbours += 1
+                If areas(Area).TryAdd((Node.x, Node.y + 1), 0) Then
+                    hoeken += FindNeighboursAndHoeken((Node.x, Node.y + 1), Area, 3)
+                    If richting <> 3 Then
+                        hoeken += 1
+                    End If
+                End If
+            End If
+        End If
+
+        areas(Area)(Node) = 4 - aantalNeighbours
+
+        Return hoeken
+
+    End Function
+
+
+    Private Sub FindNeighbours(Node As (x As Integer, y As Integer), Area As Integer)
+        Dim aantalNeighbours As Integer = 0
+        Dim Teken As Char = Map(Node)
+
+        If Not areas(Area).TryAdd(Node, 0) Then
+            Exit Sub
+        End If
+
+        If Node.x > 0 Then
+            If Map((Node.x - 1, Node.y)) = Teken Then
+                FindNeighbours((Node.x - 1, Node.y), Area)
+                aantalNeighbours += 1
+            End If
+        End If
+        If Node.x < upperX Then
+            If Map((Node.x + 1, Node.y)) = Teken Then
+                FindNeighbours((Node.x + 1, Node.y), Area)
+                aantalNeighbours += 1
+            End If
+        End If
+        If Node.y > 0 Then
+            If Map((Node.x, Node.y - 1)) = Teken Then
+                FindNeighbours((Node.x, Node.y - 1), Area)
+                aantalNeighbours += 1
+            End If
+        End If
+        If Node.y < upperY Then
+            If Map((Node.x, Node.y + 1)) = Teken Then
+                FindNeighbours((Node.x, Node.y + 1), Area)
+                aantalNeighbours += 1
+            End If
+        End If
+
+        areas(Area)(Node) = 4 - aantalNeighbours
+
+    End Sub
+
+
+End Module
