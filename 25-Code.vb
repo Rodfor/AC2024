f47ce55e7c2d0c3ce960d9a0543dee9c6670faba
Rodfor <matthias.leys@gmail.com>
2025-01-10 11:55:55 +0100

    + dag 25

---
+++ 25-Code.vb
@@ -0,0 +1,67 @@
+﻿Imports System.IO
+Imports System.Threading
+
+Module Code
+    Public Sub Unlock()
+        Dim fileName As String = "Code.txt"
+        Dim projectPath As String = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName
+        Dim Filepath As String = Path.Combine(projectPath, fileName)
+
+        Dim Lijnen = File.ReadAllLines(Filepath)
+        Dim Locks As New List(Of List(Of Integer))
+        Dim Keys As New List(Of List(Of Integer))
+
+        Dim newPattern As Boolean = True
+        Dim Teken = Nothing
+        Dim currentPattern As New List(Of Integer)
+
+        For Each L In Lijnen
+            If L = "" Then
+                newPattern = True
+                If Teken = "#" Then
+                    Locks.Add(currentPattern)
+                Else
+                    Keys.Add(currentPattern)
+                End If
+                Continue For
+            End If
+
+            If newPattern Then
+                currentPattern = New List(Of Integer)
+                Teken = L.First
+                currentPattern.AddRange(Enumerable.Repeat(-1, L.Length).ToArray())
+                newPattern = False
+            End If
+
+            For i = 0 To L.Count - 1
+                If L(i) = "#" Then currentPattern(i) += 1
+            Next
+        Next
+
+        If Teken = "#" Then
+            Locks.Add(currentPattern)
+        Else
+            Keys.Add(currentPattern)
+        End If
+
+        Dim count As Integer = Locks.Count * Keys.Count
+
+        For Each L In Locks
+            For Each k In Keys
+                For i = 0 To k.Count - 1
+                    If L(i) + k(i) > 5 Then
+                        count -= 1
+                        Exit For
+                    End If
+                Next
+            Next
+        Next
+
+        Console.WriteLine(count)
+
+    End Sub
+
+
+
+
+End Module
--- Program.vb
+++ Program.vb
@@ -4,7 +4,7 @@ Public Module Program
 
     Sub Main(args As String())
         Dim stopwatch As Stopwatch = Stopwatch.StartNew()
-        Wires()
+        Unlock()
 
         stopwatch.Stop()
         Console.WriteLine("In " + stopwatch.ElapsedMilliseconds.ToString + "ms")
